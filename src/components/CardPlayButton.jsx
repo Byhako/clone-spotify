9869a0161df04e839dcb60e073b491cc449106a3
Ruben E Acosta <reacosta@grupo-exito.com>
2023-11-26 17:10:56 -0500

    playlist card

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -11,6 +11,7 @@ export default function CardPlayButton({ id }) {
 
   const isPlayingPlalist = isPlaying && currentMusic?.playlist?.id === id
 
+
   const handleClick = () => {
     if (isPlayingPlalist) {
       setIsPlaying(false)
@@ -25,12 +26,12 @@ export default function CardPlayButton({ id }) {
       })
   }
 
-
   return (
     <button
       onClick={handleClick}
-      className='card-play-button rounded-full bg-green-500 p-4'
+      className='card-play-button rounded-full bg-green-500 p-4 hover:bg-green-400'
     >
+      {isPlayingPlalist && <p className='hidden'>si</p>}
       {isPlayingPlalist ? <Pause /> : <Play />}
     </button>
   )
